498ee0be49ba83200ba68f4a7cb5d26409c187e7
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2024-06-18 17:32:44 +0800

    chore: reformat

--- clip/clip/loss.py
+++ clip/clip/loss.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
 
+
 class CLIPLoss(nn.Module):
     def __init__(self, batch_size: int):
         super().__init__()
         self.batch_size = batch_size
-        self.label= torch.arange(0, self.batch_size)
+        self.label = torch.arange(0, self.batch_size)
 
     def forward(self, img_log, txt_log):
         img_loss = nn.CrossEntropyLoss()
@@ -14,5 +15,5 @@ class CLIPLoss(nn.Module):
         # Loss function
         loss_images = img_loss(img_log, self.label)
         loss_text = txt_loss(txt_log, self.label)
-        loss = (loss_images + loss_text)/2
+        loss = (loss_images + loss_text) / 2
         return loss
